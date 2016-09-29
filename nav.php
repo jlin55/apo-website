67fcda9ec80cb047b7dee944786573b614b5e8e4
jlin55 <jyl6557@truman.edu>
2016-09-28 20:28:39 -0500

    add a nav bar
    googe, cal, google drive, paypal, family flower, regular service, large service

--- nav.php
+++ nav.php
@@ -42,12 +42,29 @@
                 <ul>
                     <li><a href="service_admin.php">service admin</a></li>
                     <li><a href="service_admin_week.php">service admin week</a></li>
-  
                     <li><a href="attendance_admin.php">Record Attendance</a></li>
                     <li><a href="attendance_check.php">Check Attendance</a></li>
                     <li><a href="check_hours.php">Check Hours</a></li>
                     <li><a href="edit_exec.php">Edit Exec</a></li>
                     <li><a href="member_statuses.php">Edit Members</a></li>
+                </ul>
+        </li>
+  </ul>
+
+
+ <li>
+            <a href="?Links" aria-haspopup="true"><span>Links</span></a>
+                <ul>
+                    <li><a href="https://calendar.google.com/calendar/render?cid=u3q6udtat5aeia3jbfna05qga0@group.calendar.google.com#main_7">Google Calendar</a></li>
+                   <li><a href="https://drive.google.com/drive/folders/0B64sX_KlRJvYZG5Za3dDakVzYU0">Google Drive</a></li> 
+                  
+                  <li><a href="https://docs.google.com/spreadsheets/d/11J6o0GyKgN5c9_4CY68fO7Ne0u6hngnNez7_K19iE-M/edit#gid=0">Family Flower</a></li>
+  
+                    <li><a href="https://www.paypal.com/us/webapps/mpp/sem/account-selection-signup?&PID1=s3eYnkLdE|dc;117972804848;paypal;e;;eu84e2fpi0;&kw=paypal&adgroup=GT+-+PayPal+-+ONLY+%28e%29&creative=117972804848&matchtype=e&PID2=3eYnkLdE&network=g&adposition=1t1&target=&devicemodel=&campaign=GT+-+PayPal+-+ONLY&adtype=&geo_region=NA&geo_country=US&ct=SMB&act=PayPal+SMB+Google&obj=Acquisition&ch=SEM&tt=TM&mv=google&feeditemid=&campaignid=153380408&adgroupid=6407463608&loc_physical_ms=9022987&loc_interest_ms=&targetid=kwd-15051880&slid=&gclid=CLbL-Yuys88CFQYLaQodc30E7g&mpch=ads&rftp=4&srce=cnvr&ercc=27722-205546-2056-2&spid=106303560685321613
+">Paypal</a></li>
+                    <li><a href="https://docs.google.com/spreadsheets/d/1K_rUuRX0DNtQ930iT3nfpDwQxr53o_MFstckAl3AGlc/edit#gid=0">Large Service</a></li>
+                    <li><a href="https://docs.google.com/spreadsheets/d/1tGFRJCyG3HP0njM2O2MODeCQ9mVgvSVVQwxUfy4RSxQ/edit#gid=1343545188">Regular Service</a></li>
+                   
                 </ul>
         </li>
   </ul>
